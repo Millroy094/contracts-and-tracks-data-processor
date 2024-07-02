51e608b1dbe6eaf1a8f5517dd445fbbaa06f3799
Millroy094 <58091953+Millroy094@users.noreply.github.com>
2024-07-02 22:59:51 +0100

    refactor: clean up

--- src/index.ts
+++ src/index.ts
@@ -1,76 +1,30 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import Importer from "./importer";
-import { TrackRecord } from "./types";
-import { Contract, Track } from "./schema";
+import readline from 'readline';
+import fs from 'fs';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+import importTracks from './xlsx-processor';
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_DB_URL);
 
-const getTrackContract = async (
-  contractName: string
-): Promise<mongoose.Document> => {
-  const trackContract = await Contract.findOne({
-    name: contractName,
-  });
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-  if (!trackContract) {
-    throw new Error("Contract not found");
+rl.setPrompt('Please Enter a full path of the excel file to process: ');
+rl.prompt();
+
+rl.on('line', async (path) => {
+  if (!fs.existsSync(path) || !path.toLowerCase().endsWith('.xlsx')) {
+    console.log('Invalid file path or file.');
+    rl.prompt();
+  } else {
+    const errors = await importTracks(path);
+    console.error({ errors });
+    rl.close();
   }
-
-  return trackContract;
-};
-
-const importTracks = async () => {
-  await Contract.findOneAndUpdate(
-    { name: "Contract 1" },
-    { name: "Contract 1" },
-    { upsert: true, new: true }
-  );
-
-  const tracks: TrackRecord[] = await Importer.importTracks(
-    `${__dirname}/test.xlsx`
-  );
-
-  if (tracks?.length === 0) {
-    throw new Error("No tracks to process");
-  }
-
-  const errors: string[] = [];
-
-  for (const [index, track] of tracks.entries()) {
-    try {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const data: any = {
-        title: track.title,
-        isrc: track.isrc,
-        artist: track.artist,
-        version: track.version,
-        aliases: track.aliases,
-        p_line: track.p_line,
-      };
-
-      const { contract: contractName } = track;
-
-      if (contractName) {
-        const trackContract = await getTrackContract(contractName);
-        data.contract_id = trackContract;
-      }
-
-      await Track.findByIdAndUpdate(track.id, data, {
-        upsert: true,
-        new: true,
-      });
-    } catch (error) {
-      errors.push(
-        `there is an issue uploading data for row ${index + 3}. Error - ${
-          error.message
-        }`
-      );
-    }
-  }
-  console.log(errors);
+}).on('close', function () {
   process.exit(0);
-};
-
-importTracks();
+});
