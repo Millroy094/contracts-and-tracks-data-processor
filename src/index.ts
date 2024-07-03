06cee1f89c1b62d70a67c87d906bc5a90f0d6c49
Millroy Fernandes <132992164+mfernandes-alcumus@users.noreply.github.com>
2024-07-03 12:34:20 +0100

    chore: tweaks

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import readline from 'readline';
-import fs from 'fs';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import readline from "readline";
+import fs from "fs";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-import importTracks from './xlsx-processor';
+import importTracks from "./xlsx-processor";
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_DB_URL);
@@ -13,18 +13,18 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-rl.setPrompt('Please Enter a full path of the excel file to process: ');
+rl.setPrompt("Please Enter a full path of the excel file to process: ");
 rl.prompt();
 
-rl.on('line', async (path) => {
-  if (!fs.existsSync(path) || !path.toLowerCase().endsWith('.xlsx')) {
-    console.log('Invalid file path or file.');
+rl.on("line", async (path) => {
+  if (!fs.existsSync(path) || !path.toLowerCase().endsWith(".xlsx")) {
+    console.error("Invalid file path or file.");
     rl.prompt();
   } else {
     const errors = await importTracks(path);
     console.error({ errors });
     rl.close();
   }
-}).on('close', function () {
+}).on("close", function () {
   process.exit(0);
 });
