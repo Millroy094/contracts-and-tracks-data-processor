e078c280f1bf0c550b77de1de362f77e3b074192
Millroy Fernandes <132992164+mfernandes-alcumus@users.noreply.github.com>
2024-07-04 09:54:58 +0100

    chore: enhancements to unit tests

--- src/tests/xlsx-processor/index.test.ts
+++ src/tests/xlsx-processor/index.test.ts
@@ -2,6 +2,7 @@ import { describe, it, before, afterEach, after } from "mocha";
 import { expect } from "chai";
 import InMemoryMongoDb from "../in-memory-mongodb";
 import importTracks from "../../xlsx-processor";
+import { Track } from "../../schema";
 
 describe("Tracks & Contract Excel File Processor", () => {
   before(async () => await InMemoryMongoDb.connect());
@@ -14,12 +15,18 @@ describe("Tracks & Contract Excel File Processor", () => {
     const path = `${__dirname}/test-excel-files/invalid-headers.xlsx`;
     const errors = await importTracks(path);
     expect(errors).to.eql(["Invalid column headers"]);
+
+    const tracks = await Track.find();
+    expect(tracks.length).to.equal(0);
   });
 
   it("should error when there is no data supplier", async () => {
     const path = `${__dirname}/test-excel-files/no-data.xlsx`;
     const errors = await importTracks(path);
     expect(errors).to.eql(["No tracks to process"]);
+
+    const tracks = await Track.find();
+    expect(tracks.length).to.equal(0);
   });
 
   it("should error when on rows that have an invalid contract name supplied", async () => {
@@ -28,6 +35,9 @@ describe("Tracks & Contract Excel File Processor", () => {
     expect(errors).to.eql([
       "there is an issue uploading data for row 4. Error - Contract not found",
     ]);
+
+    const tracks = await Track.find();
+    expect(tracks.length).to.equal(1);
   });
 
   it("should error when on rows that missing mandatory information", async () => {
@@ -37,11 +47,17 @@ describe("Tracks & Contract Excel File Processor", () => {
       "there is an issue uploading data for row 3. Error - Track validation failed: isrc: isrc is required",
       "there is an issue uploading data for row 4. Error - Track validation failed: title: title is required",
     ]);
+
+    const tracks = await Track.find();
+    expect(tracks.length).to.equal(0);
   });
 
   it("should process all rows without errors when all information provided is correct", async () => {
     const path = `${__dirname}/test-excel-files/valid-data.xlsx`;
     const errors = await importTracks(path);
     expect(errors).to.eql([]);
+
+    const tracks = await Track.find();
+    expect(tracks.length).to.equal(2);
   });
 });
